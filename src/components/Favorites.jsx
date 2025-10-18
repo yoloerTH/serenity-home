5144ef3e82ad18626ff8bb52af183342a033dcc9
yoloerTH <amathelw2@gmail.com>
2025-10-18 22:32:27 +0300

    Update Favorites.jsx

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,5 +1,5 @@
-import React from ‘react’;
-import { Heart, ShoppingCart, Star, Trash2, Sparkles } from ‘lucide-react’;
+import React from 'react';
+import { Heart, ShoppingCart, Star, Trash2, Sparkles } from 'lucide-react';
 
 const Favorites = ({
 wishlist,
